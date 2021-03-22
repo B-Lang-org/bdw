039991ffba152512659bd8e98b1de9d71563b431
Julie Schwartz <quark@bluespec.com>
2021-03-21 20:29:52 -0400

    Only test with Itk3
    until bdw can be made to work with Itk4

--- .github/workflows/install_dependencies_testsuite_ubuntu.sh
+++ .github/workflows/install_dependencies_testsuite_ubuntu.sh
@@ -2,9 +2,12 @@
 
 apt-get update
 
-# Install the latest version of itcl/itk -- itk3 or tk-itk4.
+# The name of the itk package to install
 # NB itk depends on itcl, tk and tcl, so no need to install those separately
-ITK_PKG=$(apt-cache search -n '^(tk-)?itk[0-9]-dev' | cut -f1 -d' ' | sort | tail -1)
+#
+# For the moment, bdw only works with itk3
+#ITK_PKG=$(apt-cache search -n '^(tk-)?itk[0-9]-dev' | cut -f1 -d' ' | sort | tail -1)
+ITK_PKG=itk3
 
 apt-get install -y \
     ccache \
